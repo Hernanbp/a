dbcf8c6cffcd836a6e9ae8a7ece2133be585dbce
Hernan <hernanbp.hb@gmail.com>
2023-02-24 13:10:51 -0300

    t

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,29 +8,59 @@ gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   const paraRef = useRef(null);
+  const paraRef2 = useRef(null);
 
   useEffect(() => {
     const para = paraRef.current;
+    const para2 = paraRef2.current;
 
-    gsap.from(para, {
-      x: "-100%",
-      opacity: 0,
-      duration: 1,
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: ".h-screen",
-        start: "top center",
-        end: "bottom center",
-        scrub: 1,
+    gsap.fromTo(
+      para,
+      {
+        opacity: 0,
+        x: -100,
       },
-    });
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: para,
+          start: "top 80%",
+          end: "bottom 20%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      para2,
+      {
+        opacity: 0,
+        x: 100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: para,
+          start: "top 80%",
+          end: "bottom 20%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
   }, []);
 
   return (
     <section className="h-screen bg-[#282728]">
-      <div className="justiy-center mx-auto flex h-full w-[1580px] max-w-[90%] items-center">
+      <div className="mx-auto flex h-full w-[1580px] max-w-[90%] items-center justify-center border-4">
         <p ref={paraRef} className="text-4xl font-bold text-white">
           Sobre mi
+        </p>
+        <p ref={paraRef2} className="text-4xl font-bold text-white">
+          Lorem ipsum dolor sit amet.
         </p>
       </div>
     </section>
